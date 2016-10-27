93521f7b3007d2ed2d5f3f74f9b7e434971a91dd
Kiran Gadhave <kiran.gadhave@outlook.com>
2016-10-27 14:23:46 +0530

    Added Extension Methods

---
+++ WpfUtilLibrary/ExtensionMethods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfUtilLibrary
+{
+    public static class ExtensionMethods
+    {
+        public static bool IsAnyOf<T>(this T source, params T[] list)
+        {
+            if (null == source) throw new ArgumentNullException(nameof(source));
+            return list.Contains(source);
+        }
+
+        public static void AddRange<T, S>(this ICollection<T> list, params S[] values) where S:T
+        {
+            foreach (S value in values)
+                list.Add(value);
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> list, Action<T> mapFunction)
+        {
+            foreach (T t in list) mapFunction?.Invoke(t);
+        }
+
+        public static bool IsNull(this object o)
+        {
+            return (null == o);
+        }
+    }
+}
